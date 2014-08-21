e8039ae8b516af4cc0af1cce1edf90d450640a58
Erick Daniszewski <edaniszewski@gmail.com>
2014-08-20 21:32:54 -0400

    outline another networking class

--- src/net.py
+++ src/net.py
@@ -9,8 +9,50 @@ Created on Aug 13, 2014
 '''
 import SocketServer
 import threading
+import socket
 
-
+class Net():
+    '''
+    A socket networking object
+    '''
+    def __init__(self):
+        pass
+    
+    def getNewTCPSocketConnection(self, host, port):
+        '''
+        Create a new socket object, connect to the given host on the given port
+        and return the socket object
+        '''
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.connect((host, port))
+        return s
+        
+        
+    def send(self, sock, data):
+        '''
+        Send data over a given socket
+        '''
+        sock.sendall(data)
+        
+        
+    def getNewSocketAndSend(self, host, port, data):
+        '''
+        Create a new socket and send data over the socket
+        '''
+        self.send(self.getNewTCPSocketConnection(host, port), data)
+        
+        
+    def receive(self, sock):
+        '''
+        Receive data from a socket connection
+        '''
+        #TODO: This is just a dummy receive for now. Will need to implement a real one once protocol is further developed.
+        data = sock.recv(2048)
+        return data
+        
+        
+        
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     '''
     A TCP Server class with threading capabilities
