01f4af26ea5cae90a4dd04d6b700a668f77e91fa
Erick Daniszewski <edaniszewski@gmail.com>
2014-11-06 18:37:15 -0500

    implement send/recv with definitive data sizes

--- src/net.py
+++ src/net.py
@@ -9,12 +9,47 @@ Created on Aug 13, 2014
 """
 import socket
 import logging
+import struct
 
 import config
 
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("net_logger")
+
+
+class BaseServer:
+    """
+    A class to act as the base server for the ChunkServer and MasterServer classes.
+    Contains methods for network send and recieve.
+    """
+    def __init__(self):
+        self.struct = struct.Struct('!L')
+
+    def send(self, socket, data):
+        """
+        Send method which first sends the length of the data
+        """
+        data_length = len(data)
+        socket.send(self.struct.pack(data_length))
+
+        total_sent = 0
+        while total_sent < data_length:
+            sent = socket.send(data[total_sent:])
+            if not sent:
+                raise RuntimeError("Socket connection was broken.")
+            total_sent += sent
+
+    def recv(self, socket):
+        """
+        Receive method which first gets the length of the file to receive
+        """
+        data = ""
+        data_length = self.struct.unpack(socket.recv(4))[0]
+        while len(data) < data_length:
+            data += socket.recv(data_length - len(data))
+
+        return data
 
 
 class UDP:
@@ -52,7 +87,7 @@ class UDP:
         return data
 
 
-class ChunkServer():
+class ChunkServer(BaseServer):
     """
     Base server class for chunkservers. Handles all networking logic that chunkservers use.
     """
@@ -61,6 +96,7 @@ class ChunkServer():
         # FIXME: Are the chunkservers on the same port? They should be a different host
         # since they should be running from a seperate machine, but I suppose that shouldn't
         # matter too much.
+        super(ChunkServer, self).__init__()
         self.port = config.PORT
         self.host = config.HOST
         self.sock = None
@@ -83,13 +119,14 @@ class ChunkServer():
             print "Error value: " + str(value)
 
 
-class MasterServer():
+class MasterServer(BaseServer):
     """
     Base server class for the Master class. Handles all networking logic that the Master class
     uses. MasterServer is a multi-threaded TCP server.
     """
 
     def __init__(self):
+        super(MasterServer, self).__init__()
         self.port = config.PORT
         self.host = config.HOST
         self.sock = None
